5fdeacd1926298192b3f40b951397378ecba95ff
sermetm <eargaerg@aergaerg.com>
2025-01-07 14:05:29 +0100

    projet pays

---
+++ app.js
@@ -0,0 +1,27 @@
+const fetchCountries = async () => {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/region/europe');
+        const countries = await response.json();
+        let countriesList = document.querySelector('#card-pays');
+        countriesList.innerHTML = countries
+            .map((country)=>`<div class="card col-6 m-2 me-3" style="width: 19rem;">
+                                        <img src="${country.flags.svg}" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${country.name.common}</h5>
+                                            <p class="card-text"><span class="fw-bold">Capitale : </span>${country.capital}</p>
+                                            <p class="card-text"><span class="fw-bold">Population : </span>${country.population}</p>
+                                        </div>
+                                    </div>`)
+            .join('')
+    } catch (erreur) {
+        console.error(erreur)
+    }
+}
+
+fetchCountries()
+
+const countriesList = document.querySelector('#card-pays')
+const card = document.querySelector('#details-pays')
+countriesList.addEventListener("click",(e) => {
+    card.innerHTML = e.target.closest('.card').innerHTML
+})
